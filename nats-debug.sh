372b3e8402a89ab4142a0a5e06388e20f64f6208
gcoon151 <isptech151@yahoo.com>
2017-03-03 15:32:29 -0500

    Update nats-debug.sh

--- nats-debug.sh
+++ nats-debug.sh
@@ -11,7 +11,7 @@ nats_sub_cmd=`ls /var/vcap/jobs/*/packages/ruby*/bin/nats-sub|grep -m 1 nats-sub
 natsmessages=router.register
 workdir=/var/vcap/data
 outfile=$workdir/nats-stream.out
-observation_period=10
+observation_period=30
 # Find where we're connected to on port 4222 and exclude the 10. network because
 # That's the bosh director/agent
 nats_server=`netstat -an|grep :4222\ |grep -v 10.|awk {'print $5'}`
@@ -23,6 +23,16 @@ nats_url="nats://nats:$nats_password@$nats_server"
 echo "creating $workdir if it doesn't exist."
 mkdir -p -- "$workdir"
 
-# Debugging I always have it print what I'm going to run first as a dry run.
-#echo "( /var/vcap/packages/ruby_2.3.1/bin/nats-sub "$natsmessages" -s $nats_url > $natsdumptxt ) & sleep $observation_period ; kill $!"
-( $nats_sub_cmd "$natsmessages" -s $nats_url > $outfile ) & sleep $observation_period ; kill $!
+tail -f access.log | awk '$8 =="404" {print $1}' |egrep -v "imfmobileanalytics|api.ng.bluemix.net" >$workdir/grepfile.out & 
+grepfile_pid=$!
+
+$nats_sub_cmd "$natsmessages" -s $nats_url > $outfile &
+
+nats_sub_pid=$!
+
+date
+( grep -f $workdir/grepfile.out $outfile ) & sleep $observation_period ; kill $!
+date
+
+kill $grepfile_pid
+kill $nats_sub_pid
